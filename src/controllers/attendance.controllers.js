21c4e15e70de5edbb6276b69785b926e60f098d1
francopastor1998 <francopastorbanegas@hotmail.com>
2025-09-24 17:32:19 -0300

    add include attendance

--- src/controllers/attendance.controllers.js
+++ src/controllers/attendance.controllers.js
@@ -2,13 +2,24 @@ import database from "../config/database.js";
 
 export const getAllAttendances = async (req, res) => {
   try {
-    const attendance = await database.getClient().attendance.findMany();
+    const attendance = await database.getClient().attendance.findMany({
+      include: {
+        courseStudent: {
+          include: {
+            course: { select: { name: true } },
+            student: { select: { firstName: true, lastName: true } },
+          },
+        },
+      },
+    });
     if (!attendance || attendance.length == 0) {
       return res.status(404).json({
         message: "No se encontro la asistencia",
       });
     }
-    res.status(200).json({message:"Asistencia encontrada correctamente",attendance});
+    res
+      .status(200)
+      .json({ message: "Asistencia encontrada correctamente", attendance });
   } catch (error) {
     console.log(error);
     res.status(500).json({
